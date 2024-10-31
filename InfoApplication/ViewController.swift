8241c5bb2c6937809e633917dfd0cc47e17420cb
Oshan Maharjan <oshanmaharjan23@gmail.com>
2024-10-31 03:47:32 -0400

    Initial

--- InfoApplication/ViewController.swift
+++ InfoApplication/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var viewMoreButton: UIButton!
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    var originalBackgroundColor: UIColor?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
+        // Store the initial background color
+               originalBackgroundColor = view.backgroundColor
+        
+        // Initially hide the image and quote label
+                photoImageView.isHidden = true
+                quoteLabel.isHidden = true
+            }
+    func changeColor() -> UIColor {
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
 
-
-}
-
+            @IBAction func ButtonViewMore(_ sender: UIButton) {
+                // Toggle visibility of the image and quote label
+                photoImageView.isHidden = !photoImageView.isHidden
+                quoteLabel.isHidden = !quoteLabel.isHidden
+                
+                // Optional: Change the button title to indicate state
+                if photoImageView.isHidden {
+                    view.backgroundColor = originalBackgroundColor
+                    viewMoreButton.setTitle("VIEW MORE", for: .normal)
+                } else {
+                    viewMoreButton.setTitle("HIDE", for: .normal)
+                    view.backgroundColor = changeColor()
+                }
+            }
+        }
